7e51ad56197f0c6cd4f5b5cf57a87f6513c6aa9a
vgalante2 <vgalante0@gmail.com>
2024-04-23 21:32:44 -0400

    added seed/dummy data

--- seeds.sql
+++ seeds.sql
@@ -1,4 +1,45 @@
 
 
-SELECT * from employee 
-WHERE 
+
+INSERT INTO departments (name) VALUES
+('Marketing'),
+('Finance'),
+('Web Development'),
+('Law'),
+('Executive'),
+('Management'),
+('Human Resources'),
+('Sales');
+
+
+
+
+INSERT INTO roles (title, salary) VALUES
+('Chief Financial Officer', 120,000),
+('Recruiter', 60,000),
+('Software Developer', 105,000),
+('Digital Marketer', 70,000),
+('Account Executive', 75,000),
+('Legal Advisor', 95,000),
+('Chief Executive Officer ', 400,000),
+('Accountant', 70,000);
+
+
+
+
+INSERT INTO employees (first_name, last_name) VALUES
+('John', 'Doe'),
+('Eric', 'Johnson'),
+('Gina', 'Wilson'),
+('Rose', 'Smith'),
+('Sam', 'Caruso'),
+('Vin', 'Galante'),
+('Anthony', 'Jones'),
+('Hector', 'Davis');
+
+
+
+
+
+
+
